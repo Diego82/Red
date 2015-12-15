ba830ee14fe4f4573f722d3c90e7804f03be7c3e
diego <djtorrespe@gmail.com>
2015-12-15 14:43:53 +0100

    .

---
+++ Red/src/servidorMovil/ServidorECHO.java
@@ -0,0 +1,31 @@
+package servidorMovil;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ServidorECHO {
+
+	public static void main(String args[]) {
+
+		try {
+			ServerSocket sk = new ServerSocket(3000);
+			while (true) {
+				Socket cliente = sk.accept();
+				BufferedReader entrada = new BufferedReader(
+						new InputStreamReader(cliente.getInputStream()));
+				PrintWriter salida = new PrintWriter(
+						new OutputStreamWriter(cliente.getOutputStream()), true);
+				String datos = entrada.readLine();
+				salida.println(datos);
+				cliente.close();
+			}
+		} catch (IOException e) {
+			System.out.println(e);
+		}
+	}
+}
